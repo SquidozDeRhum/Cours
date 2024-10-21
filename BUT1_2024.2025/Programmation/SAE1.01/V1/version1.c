c0ea4fb3d6b7499e3af3fb4872ead69a57a47eb9
Squidos179 <henri457.aubree@gmail.com>
2024-10-21 19:18:26 +0200

    cxcsfvsd

--- BUT1_2024.2025/Programmation/SAE1.01/V1/version1.c
+++ BUT1_2024.2025/Programmation/SAE1.01/V1/version1.c
@@ -22,23 +22,39 @@ void gotoXY(int x, int y);
 int kbhit();
 void afficher(int x, int y, char c);
 void effacer(int x, int y);
-void dessineSerpent(int laPosition[N][2]);
-void progresser(int laPosition[N][2]);
+void dessineSerpent(int laPosition[2][N]);
+void progresser(int laPosition[2][N]);
 
 int main() {
 
     int positions[2][N];
+	int car;
 
     printf("Veuillez donner la position x de la tête : ");
-    scanf("%d", positions[0][0]);
+    scanf("%d", &positions[0][0]);
     printf("Veuillez donner la position y de la tête : ");
-    scanf("%d", positions[0][1]);
+    scanf("%d", &positions[0][1]);
 
-    system("clear");
-    afficher(1, 1, '*');
-    printf("\n");
+	for (int i = 1; i < N; i++) {
+		positions[i][0] = positions[i - 1][0] - 1;
+		positions[i][1] = positions[0][1];
+	}
 
+	while (car != 'a')
+	{
+		progresser(positions);
+		usleep(999999);
+		if (kbhit()) {
+			car = getchar();
+		}
+	}
+	
+	system("clear");
     return EXIT_SUCCESS;
+}
+
+void gotoXY(int x, int y) { 
+    printf("\033[%d;%df", y, x);
 }
 
 void afficher(int x, int y, char c) {
@@ -46,9 +62,6 @@ void afficher(int x, int y, char c) {
     printf("%c", c);
 }
 
-void gotoXY(int x, int y) { 
-    printf("\033[%d;%df", y, x);
-}
 
 int kbhit(){
 	// la fonction retourne :
@@ -79,4 +92,23 @@ int kbhit(){
 		unCaractere=1;
 	} 
 	return unCaractere;
+}
+
+void dessineSerpent(int laPosition[2][N]) {
+	system("clear");
+	for (int i = 1; i < N; i++) {
+			if (laPosition[i][0] > 0 && laPosition[i][1] > 0) {
+				afficher(laPosition[i][0], laPosition[i][1], 'o');
+			}
+		}
+	afficher(laPosition[0][0], laPosition[0][1], '*');
+	gotoXY(TAILLE, TAILLE);
+	fflush(stdout);
+}
+
+void progresser(int laPosition[2][N]) {
+	dessineSerpent(laPosition);
+	for (int i = 0; i < N; i++) {
+			laPosition[i][0]++;
+	}
 }
